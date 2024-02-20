0f12099617f67bbf989ca9a289cef6f46622ac6f
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2024-02-20 11:41:29 -0300

    add and remove buttons

--- view.js
+++ view.js
@@ -1,10 +1,17 @@
-
-function View(timerElementParam){
+function View(startParam, pauseParam, resetParam, timerElementParam){
+    let start = startParam;
+    let pause = pauseParam;
+    let reset = resetParam;
     let timerElement = timerElementParam;
     let stateTime = 'reset';
     let oCronometer = null;
 
     return {
+        init(){
+            start.addEventListener('click', this.start.bind(this));
+            pause.addEventListener('click', this.pause.bind(this));
+            reset.addEventListener('click', this.reset.bind(this));
+        },
         setCronometer(oCronometerParam) {
             oCronometer = oCronometerParam;
         },
@@ -13,21 +20,37 @@ function View(timerElementParam){
             const ssmmhh = (s, m, h) => `${addZero(h)}:${addZero(m)}:${addZero(s)}`;
             timerElement.innerText = ssmmhh(seg, min, hour);
         },
+        permutButtons(state) {
+            stateTime = state;
+            switch (state) {
+                case 'reset':
+                    reset.classList.add('hidden');
+                case 'pause':
+                    pause.classList.add('hidden');
+                    start.classList.remove('hidden');
+                    break;
+                case 'start':
+                    start.classList.add('hidden');
+                    pause.classList.remove('hidden');
+                    reset.classList.remove('hidden');
+                    break;
+            }
+        },
         start() {
             if(stateTime === 'pause' || stateTime === 'reset') {
-                stateTime = 'start';
+                this.permutButtons('start');
                 oCronometer.start();
             }
         },
         pause() {
             if(stateTime === 'start') {
-                stateTime = 'pause';
+                this.permutButtons('pause');
                 oCronometer.pause();
             }
         },
         reset() {
             if(stateTime === 'start' || stateTime === 'pause') {
-                stateTime = 'reset';
+                this.permutButtons('reset');
                 oCronometer.reset();
             }
         }
