f4027dceb2c21e0bf359f6869b988b1c00bcc347
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2024-02-20 13:18:39 -0300

    feature, add - vueltas

--- view.js
+++ view.js
@@ -1,14 +1,17 @@
-function View(startPauseParam, resetParam, timerElementParam){
-    let startPause = startPauseParam;
+function View(startPauseParam, resetParam, timerElementParam, lapsParam){
+    let startPauseElement = startPauseParam;
     let reset = resetParam;
     let timerElement = timerElementParam;
+    let lapElement = lapsParam.button;
+    let lapsList = lapsParam.list;
     let stateTime = 'pause';
     let oCronometer = null;
 
     return {
         init(){
-            startPause.addEventListener('click', this.startPause.bind(this));
-            reset.addEventListener('click', this.reset.bind(this));
+            startPauseElement.addEventListener('click', this.handler.bind(this));
+            reset.addEventListener('click', this.handler.bind(this));
+            lapElement.addEventListener('click', this.handler.bind(this));
         },
         setCronometer(oCronometerParam) {
             oCronometer = oCronometerParam;
@@ -18,16 +21,28 @@ function View(startPauseParam, resetParam, timerElementParam){
             const ssmmhh = (s, m, h) => `${addZero(h)}:${addZero(m)}:${addZero(s)}`;
             timerElement.innerText = ssmmhh(seg, min, hour);
         },
+        handler(e) {
+            const button = e.target.name;
+            console.log(button);
+
+            switch (button) {
+                case 'reset': this.reset(); break;
+                case 'startPause': this.startPause(); break;
+                case 'lap': this.laps(); break;
+            }
+        },
         startPause(){
             switch (stateTime) {
                 case 'pause':
                     stateTime = 'start';
                     reset.classList.remove('hidden');
+                    lapElement.classList.remove('hidden');
                     oCronometer.start();
                     break;
                 case 'start':
                     stateTime = 'pause';
                     reset.classList.remove('hidden');
+                    lapElement.classList.add('hidden');
                     oCronometer.pause();
                     break;
             }
@@ -35,7 +50,24 @@ function View(startPauseParam, resetParam, timerElementParam){
         reset() {
             stateTime = 'pause';
             reset.classList.add('hidden');
+            lapElement.classList.add('hidden');
             oCronometer.reset();
+            this.removeLaps();
+        },
+        laps(){
+
+            const addLap = lap => {
+                const li = document.createElement('li');
+                li.innerText = lap;
+                lapsList.appendChild(li);
+            }
+
+            addLap(timerElement.innerText);
+        },
+        removeLaps(){
+            while(lapsList.children.length > 0){
+                lapsList.removeChild(lapsList.lastChild);
+            }
         }
     }
 }
